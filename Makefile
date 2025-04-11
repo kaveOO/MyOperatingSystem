4d476e07992a1abf360542ec5a84b06288daf5ae
kaveO <alban.billiette@gmail.com>
2025-04-11 15:03:30 +0200

    + | clean (Makefile) + coms (main.asm)

--- Makefile
+++ Makefile
@@ -9,3 +9,6 @@ $(BUILD_DIR)/main_floppy.img: $(BUILD_DIR)/main.bin
 
 $(BUILD_DIR)/main.bin: $(SRC_DIR)/main.asm
 	$(ASM) $(SRC_DIR)/main.asm -f bin -o $(BUILD_DIR)/main.bin
+
+clean:
+	rm $(BUILD_DIR)/*
--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,11 @@
-org 0x7C00
-bits 16
+org 0x7C00 // Bios load address, start address out here !
+bits 16 // CPU always start in 16 bits mode.
 
 main:
-	hlt
+	hlt // Halt, stops the CPU execution.
 
 .halt
-	jmp .halt
+	jmp .halt // In case hlt get skipped, infinite loop to avoid CPU from going crazy :D
 
-times 510-($-$$) db 0
-dw 0AA55h
+times 510-($-$$) db 0 // Write all bytes to 0 until 510 bytes (to ensure the left space is BOOT SIGNATURE)
+dw 0AA55h // Adds the BOOT SIGNATURE
